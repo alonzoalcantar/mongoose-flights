a81758c1e0dfc4132661dfafe39454671189b7d0
alonzoalcantar <pinoa.03@outlook.com>
2022-11-02 04:47:03 -0700

    flight list displays with data

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,10 +1,35 @@
 //   controllers/flights.js
 
+const Flight = require('../models/flights')
+
 module.exports = {
-    new: newFlight
+    new: newFlight,
+    create,
+    index
 };
+
 
 
 function newFlight(req,res) {
     res.render('flights/new');
+}
+
+function create (req, res) {
+    const flight = new Flight(req.body);
+    flight.save(function(err){
+        if(err) return res.redirect('/flights/new');
+        console.log(flight);
+        res.redirect('/flights/new');
+    });
+}
+
+
+function index (req, res) {
+    Flight.find({}, function(err,flights){
+        if(err){
+            console.log(err);
+            res.redirect('/');
+        }
+        res.render("flights/index", {flights});
+    });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,5 +4,10 @@ var flightsCtrl = require('../controllers/flights')
 
 //GET flights/new
 router.get('/new', flightsCtrl.new);
+//POST new movie 
+router.post('/', flightsCtrl.create);
+//GET all movies 
+router.get('/', flightsCtrl.index);
+
 
 module.exports = router;
