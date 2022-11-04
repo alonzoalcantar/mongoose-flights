bce2d6cb636de1b19e247ea970a1064f1d10f065
alonzoalcantar <pinoa.03@outlook.com>
2022-11-04 03:05:26 -0700

    add destination, final display bug

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -8,7 +8,7 @@ module.exports = {
 function create(req, res) {
     Flight.findById(req.params.id, function(err, flight) {
       // We can push subdocs into Mongoose arrays
-      flight.destination.push(req.body);
+      flight.destinations.push(req.body);
       // Save any changes made to the movie doc
       flight.save(function(err) {
         // Step 5:  Respond to the Request (redirect if data has been changed)
